096e5889a5c65069299d3f95d96e914611225ec0
Gian Marco Todesco <todesco@toonz.com>
2021-01-21 12:52:38 +0100

    .

---
+++ examples/moebius_2.js
@@ -0,0 +1,69 @@
+console.log(window);
+MYLIB.initialize('renderCanvas', populateScene);
+
+
+function populateScene(scene) {
+    //MYLIB.createGrid(scene);
+
+    // creo il materiale per tutti gli elementi
+    let mat = new BABYLON.StandardMaterial("mat",scene);
+    mat.diffuseColor.set(0.8,0.4,0.1);
+    mat.specularColor.set(0.2,0.2,0.2);
+
+    const pairs = [];
+    const n = 25;
+    const R = 5;
+
+    const sx = 2, sy = 0.5, sz = 2;
+
+    for(let i=0; i<n; i++) {
+		
+      let pair = new BABYLON.Mesh("b",scene);
+      pairs.push(pair);
+      
+      let element;		
+      // first element
+      element = BABYLON.MeshBuilder.CreateSphere("a", {diameter : 1 }, scene);
+      element.scaling.set(sx,sy,sz);		
+      element.material = mat;
+      
+      element.parent = pair;
+      element.position.y = 0.5;
+      pair.firstElement = element;
+      
+      // second element
+      element = BABYLON.MeshBuilder.CreateSphere("b", {diameter : 1 }, scene);
+      element.scaling.set(sx,sy,sz);		
+      element.material = mat;
+      element.parent = pair;
+      element.position.y = -0.5;		
+      pair.secondElement = element;
+      
+    }
+  
+
+    scene.registerBeforeRender(() => {
+        let t = performance.now() * 0.001;
+        var psi = t*0.1;
+        var gamma = psi * 20;
+
+        pairs.forEach((pair,i) => {
+          let phi = Math.PI*2*i/n;
+
+          pair.position.set(R*Math.cos(phi), 0, -R*Math.sin(phi));
+          pair.rotation.y = phi;
+          pair.rotation.z = phi/2 + psi;
+
+          var x = Math.sin(gamma)*1.1;
+          var y = Math.cos(gamma)*0.3;
+          
+          pair.firstElement.position.set(x,y,0); 
+          pair.secondElement.position.set(-x,-y,0); 
+      
+        });
+      
+    });
+
+
+
+}
